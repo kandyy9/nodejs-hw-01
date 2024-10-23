861e4a2821727664935aa8345eda2de8894057d0
kandy <a.kandy.sk@gmail.com>
2024-10-23 09:58:42 +0300

    hw-done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,13 @@
-const generateContacts = async (number) => {};
+import { writeContacts } from '../utils/writeContacts';
+import { createFakeContact } from './../utils/createFakeContact';
+import { readContacts } from '../utils/readContacts.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+  for (let i = 0; i < number; i++) {
+    contacts.push(createFakeContact());
+  }
+  await writeContacts(contacts);
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { writeFile } from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const data = JSON.stringify(updatedContacts, null, 2);
+  await writeFile(PATH_DB, data, 'utf8');
+};
